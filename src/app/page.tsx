fcf6f2b199efe34641e84bb639e2459c30cbff68
Khushi <khushiyadav2496301@gmail.com>
2026-06-03 17:54:21 +0530

    fix supabase server build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Sidebar from "@/components/Sidebar";
 import DashboardClient from "@/components/DashboardClient";
-import { supabase } from "@/lib/supabase";
 import type { Course } from "@/lib/types";
+import { createClient } from "@supabase/supabase-js";
 
 export default async function Home() {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  );
+
   const { data } = await supabase.from("courses").select("*");
 
   const courses: Course[] = data ?? [];
